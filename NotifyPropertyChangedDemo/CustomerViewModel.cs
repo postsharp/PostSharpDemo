704d4959a73f2c5463c8321772350dd8d38cef39
Gael Fraiteur <gael@postsharp.net>
2015-08-27 06:29:49 -0700

    Prepared sample for toolbar and validation.

--- NotifyPropertyChangedDemo/CustomerViewModel.cs
+++ NotifyPropertyChangedDemo/CustomerViewModel.cs
@@ -13,7 +13,7 @@
         {
             get
             {
-                if (this.Customer == null) return "null";
+                if (this.Customer == null) return "(No Data)";
 
                 return string.Format("{0} {1} from {2}", 
                     this.Customer.FirstName, 
